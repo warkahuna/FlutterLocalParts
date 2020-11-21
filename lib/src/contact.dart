043c263488bea252d06baafc2f7111f52d247f4c
iheb hassine <45923694+warkahuna@users.noreply.github.com>
2020-11-21 05:56:40 +0100

    contact

---
+++ lib/src/contact.dart
@@ -0,0 +1,481 @@
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:awesome_dialog/awesome_dialog.dart';
+import 'package:camera/camera.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_login_signup/src/Homepage/custom_navigation_bar.dart';
+import 'package:flutter_login_signup/src/Homepage/custome_navigation_bar.dart';
+import 'package:flutter_login_signup/src/Parts/Devoirs/Devoirs.dart';
+import 'package:flutter_login_signup/src/Widget/bezierContainer.dart';
+import 'package:flutter_login_signup/src/filter/filterlist.dart';
+import 'package:flutter_login_signup/src/navBar.dart';
+import 'package:flutter_login_signup/src/userProfile/page2/page2.dart';
+import 'package:flutter_login_signup/src/userProfile/page2/part_view.dart';
+import 'package:flutter_login_signup/src/userProfile/page2/parts_feeder.dart';
+import 'package:http/http.dart' as http;
+import 'package:image_picker/image_picker.dart';
+
+class Contact extends StatefulWidget {
+  Contact({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  _ContactState createState() => _ContactState();
+}
+
+class _ContactState extends State<Contact> {
+  int _currentIndex = 4;
+
+  List<int> _badgeCounts = List<int>.generate(5, (index) => index);
+
+  List<bool> _badgeShows = List<bool>.generate(5, (index) => true);
+
+  TextEditingController priceController = new TextEditingController();
+  TextEditingController nameController = new TextEditingController();
+  TextEditingController referanceController = new TextEditingController();
+  TextEditingController categoryController = new TextEditingController();
+  TextEditingController other1Controller = new TextEditingController();
+  TextEditingController content1Controller = new TextEditingController();
+  TextEditingController other2Controller = new TextEditingController();
+  TextEditingController content2Controller = new TextEditingController();
+  TextEditingController other3Controller = new TextEditingController();
+  TextEditingController content3Controller = new TextEditingController();
+  TextEditingController descriptionController = new TextEditingController();
+  TextEditingController imageController = new TextEditingController();
+
+  List categorys = [
+    "Brakes",
+    "Filtering/Oil,Suspension and Steering",
+    "Transmission-Gearbox",
+    "Exterior/Interior Equipment and Accessories",
+    "Engine compartment",
+    "Exhaust",
+    "Electrical and lighting",
+    "Air conditioning",
+    "Locks-closure",
+    "Tyres",
+    "Others",
+  ];
+  int _value = 1;
+
+  _imgFromCamera() async {
+    File image = await ImagePicker.pickImage(
+        source: ImageSource.camera, imageQuality: 50);
+
+    setState(() {
+      List<int> imageBytes = image.readAsBytesSync();
+      print(imageBytes);
+      String base64Image = base64Encode(imageBytes);
+      imageController.text = base64Image;
+    });
+  }
+
+  _imgFromGallery() async {
+    File image = await ImagePicker.pickImage(
+        source: ImageSource.gallery, imageQuality: 50);
+
+    setState(() {
+      List<int> imageBytes = image.readAsBytesSync();
+      print(imageBytes);
+      String base64Image = base64Encode(imageBytes);
+      imageController.text = base64Image;
+    });
+  }
+
+  void _showPicker(context) {
+    showModalBottomSheet(
+        context: context,
+        builder: (BuildContext bc) {
+          return SafeArea(
+            child: Container(
+              child: new Wrap(
+                children: <Widget>[
+                  new ListTile(
+                      leading: new Icon(Icons.photo_library),
+                      title: new Text('Photo Library'),
+                      onTap: () {
+                        _imgFromGallery();
+                        Navigator.of(context).pop();
+                      }),
+                  new ListTile(
+                    leading: new Icon(Icons.photo_camera),
+                    title: new Text('Camera'),
+                    onTap: () {
+                      _imgFromCamera();
+                      Navigator.of(context).pop();
+                    },
+                  ),
+                ],
+              ),
+            ),
+          );
+        });
+  }
+
+  Widget _buildOriginDesign() {
+    return CustomNavigationBar(
+      iconSize: 30.0,
+      selectedColor: Colors.white,
+      strokeColor: Colors.white,
+      unSelectedColor: Color(0xff6c788a),
+      backgroundColor: Color(0xff040307),
+      items: [
+        CustomNavigationBarItem(
+          icon: Icons.home,
+        ),
+        CustomNavigationBarItem(
+          icon: Icons.shopping_cart,
+        ),
+        CustomNavigationBarItem(
+          icon: Icons.lightbulb_outline,
+        ),
+        CustomNavigationBarItem(
+          icon: Icons.search,
+        ),
+        CustomNavigationBarItem(
+          icon: Icons.account_circle,
+        ),
+      ],
+      currentIndex: _currentIndex,
+      onTap: (index) {
+        switch (index) {
+          case 0:
+            Navigator.push(context,
+                MaterialPageRoute(builder: (context) => DevoirsPage()));
+            break;
+          case 1:
+            break;
+          case 2:
+            break;
+          case 3:
+            break;
+          case 4:
+            Navigator.push(
+                context, MaterialPageRoute(builder: (context) => Page2()));
+            break;
+          default:
+        }
+        setState(() {
+          _currentIndex = index;
+        });
+      },
+    );
+  }
+
+  Widget _header() {
+    return Container(
+      height: MediaQuery.of(context).size.height * 0.4,
+      child: Stack(
+        children: <Widget>[
+          Image.memory(base64Decode(imageController.text),
+              height: 1000, width: 2000, fit: BoxFit.fitWidth),
+          Container(
+            color: Colors.black45,
+          ),
+          SafeArea(
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                IconButton(
+                  icon: Icon(
+                    Icons.home,
+                    color: Colors.white,
+                  ),
+                  onPressed: () {
+                    Navigator.push(context,
+                        MaterialPageRoute(builder: (context) => DevoirsPage()));
+                  },
+                ),
+                Text(
+                  "contact",
+                  style: TextStyle(
+                    color: Colors.white,
+                  ),
+                ),
+                SizedBox(width: 32.0),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _entryField(String title, TextEditingController controller,
+      {bool isPassword = false}) {
+    return Container(
+      margin: EdgeInsets.symmetric(vertical: 10),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Text(
+            title,
+            style: TextStyle(fontWeight: FontWeight.bold, fontSize: 15),
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          TextField(
+              controller: controller,
+              obscureText: isPassword,
+              decoration: InputDecoration(
+                  border: InputBorder.none,
+                  fillColor: Colors.grey[300],
+                  filled: true))
+        ],
+      ),
+    );
+  }
+
+  @override
+  void initState() {
+    super.initState();
+
+    //_value = categorys.indexOf(categoryController.text) - 1;
+  }
+
+  Widget _submitButton() {
+    return InkWell(
+      onTap: () async {},
+      child: Container(
+        width: MediaQuery.of(context).size.width,
+        padding: EdgeInsets.symmetric(vertical: 10),
+        alignment: Alignment.center,
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.all(Radius.circular(5)),
+            boxShadow: <BoxShadow>[
+              BoxShadow(
+                  color: Colors.grey.shade200,
+                  offset: Offset(2, 4),
+                  blurRadius: 5,
+                  spreadRadius: 2)
+            ],
+            gradient: LinearGradient(
+                begin: Alignment.centerLeft,
+                end: Alignment.centerRight,
+                colors: [Color(0xff9ba1a6), Color(0xff3B3F42)])),
+        child: Text(
+          'chat',
+          style: TextStyle(fontSize: 20, color: Colors.white),
+        ),
+      ),
+    );
+  }
+
+  Widget _phoneButton() {
+    return InkWell(
+      onTap: () async {},
+      child: Container(
+        width: MediaQuery.of(context).size.width,
+        padding: EdgeInsets.symmetric(vertical: 10),
+        alignment: Alignment.center,
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.all(Radius.circular(5)),
+            boxShadow: <BoxShadow>[
+              BoxShadow(
+                  color: Colors.grey.shade200,
+                  offset: Offset(2, 4),
+                  blurRadius: 5,
+                  spreadRadius: 2)
+            ],
+            gradient: LinearGradient(
+                begin: Alignment.centerLeft,
+                end: Alignment.centerRight,
+                colors: [Color(0xff9ba1a6), Color(0xff3B3F42)])),
+        child: Text(
+          'phone',
+          style: TextStyle(fontSize: 20, color: Colors.white),
+        ),
+      ),
+    );
+  }
+
+  Widget _deleteButton() {
+    return InkWell(
+      onTap: () async {},
+      child: Container(
+        width: MediaQuery.of(context).size.width,
+        padding: EdgeInsets.symmetric(vertical: 10),
+        alignment: Alignment.center,
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.all(Radius.circular(5)),
+            boxShadow: <BoxShadow>[
+              BoxShadow(
+                  color: Colors.grey.shade200,
+                  offset: Offset(2, 4),
+                  blurRadius: 5,
+                  spreadRadius: 2)
+            ],
+            gradient: LinearGradient(
+                begin: Alignment.centerLeft,
+                end: Alignment.centerRight,
+                colors: [Color(0xff9ba1a6), Color(0xff3B3F42)])),
+        child: Text(
+          'email',
+          style: TextStyle(fontSize: 20, color: Colors.white),
+        ),
+      ),
+    );
+  }
+
+  Widget _takePicture() {
+    return GestureDetector(
+      onTap: () {
+        _showPicker(context);
+      },
+      child: Container(
+        width: MediaQuery.of(context).size.width / 7,
+        height: MediaQuery.of(context).size.height / 13,
+        alignment: Alignment.center,
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.all(Radius.circular(5)),
+            boxShadow: <BoxShadow>[
+              BoxShadow(
+                  color: Colors.grey.shade200,
+                  offset: Offset(2, 4),
+                  blurRadius: 5,
+                  spreadRadius: 2)
+            ],
+            gradient: LinearGradient(
+                begin: Alignment.centerLeft,
+                end: Alignment.centerRight,
+                colors: [Colors.white, Colors.white])),
+        child: Icon(
+          Icons.image,
+          color: Colors.grey,
+          size: 50.0,
+        ),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final height = MediaQuery.of(context).size.height;
+    return Scaffold(
+        body: Container(
+      height: height,
+      child: Stack(
+        children: <Widget>[
+          _header(),
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: Container(
+              height: MediaQuery.of(context).size.height * 0.7,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.vertical(
+                  top: Radius.circular(10.0),
+                ),
+              ),
+              child: SingleChildScrollView(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Container(
+                      margin: const EdgeInsets.all(10.0),
+                      padding: const EdgeInsets.all(10.0),
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        border: Border.all(
+                          width: 1,
+                          color: Colors.grey,
+                        ),
+                        borderRadius: BorderRadius.vertical(
+                          top: Radius.circular(10.0),
+                          bottom: Radius.circular(10.0),
+                        ),
+                      ),
+                      child: Column(
+                        children: <Widget>[],
+                      ),
+                    ),
+                    Container(
+                      margin: const EdgeInsets.all(10.0),
+                      padding: const EdgeInsets.all(10.0),
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        border: Border.all(
+                          width: 1,
+                          color: Colors.grey,
+                        ),
+                        borderRadius: BorderRadius.vertical(
+                          top: Radius.circular(10.0),
+                          bottom: Radius.circular(10.0),
+                        ),
+                      ),
+                      child: Column(
+                        children: <Widget>[
+                          Container(
+                            margin: const EdgeInsets.all(10.0),
+                            padding: const EdgeInsets.all(10.0),
+                            decoration: BoxDecoration(
+                              color: Colors.white,
+                              border: Border.all(
+                                width: 1,
+                                color: Colors.grey,
+                              ),
+                              borderRadius: BorderRadius.vertical(
+                                top: Radius.circular(10.0),
+                                bottom: Radius.circular(10.0),
+                              ),
+                            ),
+                            child: Column(
+                              children: <Widget>[
+                                _submitButton(),
+                                _deleteButton(),
+                                _phoneButton(),
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+          Expanded(
+            child: Align(
+              alignment: FractionalOffset.bottomCenter,
+              child: _buildOriginDesign(),
+            ),
+          ),
+        ],
+      ),
+    ));
+  }
+}
+
+class FollowCard extends StatelessWidget {
+  final title;
+  final subtitle;
+
+  const FollowCard({Key key, this.title, this.subtitle}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        Text(
+          title,
+          style: TextStyle(
+            color: Colors.black38,
+            fontSize: 12.0,
+          ),
+        ),
+        SizedBox(height: 2.0),
+        Text(
+          subtitle,
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+            fontSize: 18.0,
+          ),
+        ),
+      ],
+    );
+  }
+}
